91b20c1b40e6cfbb04fbfa79720857f1e7821816
girishprof-tech <girishprof65@gmail.com>
2026-08-05 21:18:47 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.78%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     void func(vector<vector<int>> &ans, vector<int>& candidates, vector<int>& temp, int ind, int sum) {
-        if (sum == 0 && !temp.empty()) {
-            ans.push_back(temp);
+        if (ind >= candidates.size()) {
+            if (sum == 0) {
+                ans.push_back(temp);
+            }
             return;
         }
         
-        if (ind >= candidates.size() || sum < 0) return;
-        
-        temp.push_back(candidates[ind]);
-        func(ans, candidates, temp, ind, sum - candidates[ind]);
-        temp.pop_back();
+        if (candidates[ind] <= sum) {
+            temp.push_back(candidates[ind]);
+            func(ans, candidates, temp, ind, sum - candidates[ind]);
+            temp.pop_back();
+        }
+
         func(ans, candidates, temp, ind + 1, sum);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
