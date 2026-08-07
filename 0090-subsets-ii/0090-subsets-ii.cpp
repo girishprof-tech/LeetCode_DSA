6f594743fe2e264252ede524d94f4b343c19a9a1
girishprof-tech <girishprof65@gmail.com>
2026-08-07 19:20:59 +0530

    Time: 7 ms (5.97%), Space: 17.4 MB (5.17%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void func(set<vector<int>> &ans, vector<int> &nums, vector<int> temp, int ind) {
         if (ind == nums.size()) {
-            sort(temp.begin(), temp.end());
             ans.insert(temp);
             return;
         }
@@ -15,8 +14,9 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> ans;
         vector<int> temp;
-        func(ans, nums, temp, 0);
+        sort(nums.begin(), nums.end());
 
+        func(ans, nums, temp, 0);
         vector<vector<int>> res(ans.begin(), ans.end());
         return res;
     }
