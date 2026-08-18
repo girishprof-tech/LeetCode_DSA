cc39b6e92fbe700e04c127d05115d00597707418
girishprof-tech <girishprof65@gmail.com>
2026-08-18 17:52:13 +0530

    Time: 4 ms (31.51%), Space: 10.2 MB (78.6%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    void func(vector<vector<string>>& ans, vector<string>& board, int n, int c, vector<int>& left, vector<int>& upper, vector<int>& lower) {
+    bool isSafe(int r, int c, vector<string>& board, int n) {
+        for (int j = c; j >= 0; j--) {
+            if (board[r][j] == 'Q') return false;
+        }
+
+        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        for (int i = r, j = c; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+    void func(vector<vector<string>>& ans, vector<string>& board, int n, int c) {
         if (n == c) {
             ans.push_back(board);
             return;
         }
 
         for (int r = 0; r < n; r++) {
-            if (left[r] == 0 && upper[n - 1 + c - r] == 0 && lower[r + c] == 0) {
+            if (isSafe(r, c, board, n)) {
                 board[r][c] = 'Q';
-                left[r] = 1;
-                upper[n - 1 + c - r] = 1;
-                lower[r + c] = 1;
-
-                func(ans, board, n, c + 1, left, upper, lower);
-
+                func(ans, board, n, c + 1);
                 board[r][c] = '.';
-                left[r] = 0;
-                upper[n - 1 + c - r] = 0;
-                lower[r + c] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n, string(n, '.'));
-        vector<int> left(n, 0), upper(2 * n - 1, 0), lower(2 * n - 1, 0);
-        func(ans, board, n, 0, left, upper, lower);
+        func(ans, board, n, 0);
         return ans;
     }
 };
