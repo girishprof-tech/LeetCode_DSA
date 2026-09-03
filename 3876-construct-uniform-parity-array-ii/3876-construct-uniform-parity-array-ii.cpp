ace7984842b5e2e6dfb2c318dcd2266d027a6c7f
Girish Garg <girishprof65@gmail.com>
2026-09-03 22:33:48 +0530

    Time: 62 ms (25.65%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkForEven(vector<int>& nums1, int n) {
+        int idx = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums1[i] % 2 == 1) {
+                idx = i;
+                break;
+            }
+        }
+
+        if (idx == -1) return true;
+        return false;
+    }
+
+    bool uniformArray(vector<int>& nums1) {
+        sort(nums1.begin(), nums1.end());
+
+        if (nums1[0] % 2 == 0)
+            return checkForEven(nums1, nums1.size());
+
+        return true;
+    }
+};
