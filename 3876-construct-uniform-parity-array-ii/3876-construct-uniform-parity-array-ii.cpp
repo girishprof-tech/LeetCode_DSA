c1190dae74b0d3623780a88b10e001064e624987
Girish Garg <girishprof65@gmail.com>
2026-09-03 23:18:35 +0530

    Time: 0 ms (100%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -3,19 +3,17 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
 
-        int minIdx = -1;
         int idx = -1;
         int mn = INT_MAX;
         for (int i = 0; i < n; i++) {
-            if (nums1[i] < mn) {
-                mn = nums1[i];
-                minIdx = i;
-            }
+            mn = min(mn, nums1[i]);
 
-            if (idx == -1 && nums1[i] % 2 == 1) idx = i;
+            if (idx == -1 && nums1[i] % 2 == 1)
+                idx = 0;
         }
 
-        if (mn % 2 == 1 || idx == -1) return true;
+        if (mn % 2 == 1 || idx == -1)
+            return true;
         return false;
     }
 };
