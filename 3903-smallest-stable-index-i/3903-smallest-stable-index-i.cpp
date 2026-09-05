46605e10f9c77191b7aad24bbaa4d7c971e4d9bc
Girish Garg <girishprof65@gmail.com>
2026-09-05 16:12:46 +0530

    Time: 0 ms (100%), Space: 30.4 MB (76.38%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -4,25 +4,18 @@ public:
         int n = nums.size();
 
         int mx = 0;
-        int mn = INT_MAX;
-
-        for (int i = 0; i < n; i++) {
-            mn = min(mn, nums[i]);
-        }
+        int cand = 0, cMax = 0;
 
         for (int i = 0; i < n; i++) {
             mx = max(mx, nums[i]);
 
-            if (i != 0 && nums[i - 1] == mn) {
-                mn = INT_MAX;
-                for (int j = i; j < n; j++) {
-                    mn = min(mn, nums[j]);
-                }
-            }
+            if (cand == i) cMax = mx;
 
-            if (mx - mn <= k) return i;
+            if (nums[i] < cMax - k) {
+                cand = i + 1;
+            }
         }
 
-        return -1;
+        return cand < n ? cand : -1;
     }
 };
