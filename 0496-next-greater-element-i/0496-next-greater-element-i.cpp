e504213723de00fa2eb614a2839fa49a0b75fb6e
Girish Garg <girishprof65@gmail.com>
2026-09-11 01:27:21 +0530

    Time: 0 ms (100%), Space: 12.9 MB (38.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,26 +3,18 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-        vector<int> temp(n2);
+        unordered_map<int, int> hash;
+        stack<int> st;
         vector<int> ans(n1);
 
-        stack<int> st;
         for (int i = n2 - 1; i >= 0; i--) {
             while (!st.empty() && st.top() <= nums2[i]) st.pop();
-
-            if (st.empty()) temp[i] = -1;
-            else temp[i] = st.top();
-
+            hash[nums2[i]] = st.empty() ? -1 : st.top();
             st.push(nums2[i]);
         }
 
         for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
-                if (nums1[i] == nums2[j]) {
-                    ans[i] = temp[j];
-                    break;
-                }
-            }
+            ans[i] = hash[nums1[i]];
         }
 
         return ans;
