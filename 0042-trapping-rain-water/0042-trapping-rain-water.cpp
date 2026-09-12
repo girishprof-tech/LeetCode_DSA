c150e460abd821ed711975794ec5332d8225e312
Girish Garg <girishprof65@gmail.com>
2026-09-12 18:00:50 +0530

    Time: 1 ms (21.88%), Space: 27.1 MB (32.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    int between(vector<int>& height, int i, int j) {
-        int mx = 0;
-        for (int t = i + 1; t < j; t++) {
-            mx = max(mx, height[t]);
-        }
-        return mx;
-    }
     int trap(vector<int>& height) {
         int n = height.size();
         if (n <= 2) return 0;
 
-        int cnt = 0;
-        stack<pair<int, int>> st;
+        int water = 0;
+        stack<int> st;
 
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && st.top().first <= height[i]) {
-                int diff = between(height, st.top().second, i);
-
-                cnt += (i - st.top().second - 1) * (st.top().first - diff);
-
+            while (!st.empty() && height[i] > height[st.top()]) {
+                int bottom = st.top();
                 st.pop();
-            }
-            
-            if (!st.empty()) {
-                int diff2 = between(height, st.top().second, i);
-                cnt += (i - st.top().second - 1) * (height[i] - diff2);
+
+                if (st.empty()) break;
+                
+                int left = st.top();
+                int width = i - left - 1;
+                int bounded = min(height[i], height[left]) - height[bottom];
+
+                water += width * bounded;
             }
 
-            st.push({height[i], i});
+            st.push(i);
         }
 
-        return cnt;
+        return water;
     }
 };
