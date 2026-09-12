49567cafcf70902eade10dce19bb08e659b2a2b6
Girish Garg <girishprof65@gmail.com>
2026-09-12 21:20:54 +0530

    Time: 0 ms (100%), Space: 26.2 MB (40.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -10,16 +10,17 @@ public:
         while (l < r) {
             if (height[l] <= height[r]) {
                 if (lMax > height[l]) {
-                    water += lMax - height[l];
+                    water += min(lMax, height[r]) - height[l];
                 }
-                else lMax = height[l];
+                lMax = max(lMax, height[l]);
                 l++;
             }
             else {
                 if (rMax > height[r]) {
-                    water += rMax - height[r];
+                    water += min(height[l], rMax) - height[r];
                 }
-                else rMax = height[r];
+
+                rMax = max(rMax, height[r]);
                 r--;
             }
         }
