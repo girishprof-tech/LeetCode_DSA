558d580ba2e1028958f95ba94d2c28627a875ff6
Girish Garg <girishprof65@gmail.com>
2026-09-12 21:18:17 +0530

    Time: 0 ms (100%), Space: 26.1 MB (55.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,21 +4,25 @@ public:
         int n = height.size();
         if (n <= 2) return 0;
 
-        int water = 0;
-        int prefix = height[0];
+        int water = 0, lMax = 0, rMax = 0;
+        int l = 0, r = n - 1;
 
-        vector<int> suffix(n);
-        suffix[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = max(suffix[i + 1], height[i]);
-        }
-
-        for (int i = 1; i < n - 1; i++) {
-            if (prefix > height[i] && suffix[i + 1] > height[i]) {
-                water += min(prefix, suffix[i + 1]) - height[i];
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                if (lMax > height[l]) {
+                    water += min(lMax, height[r]) - height[l];
+                }
+                lMax = max(lMax, height[l]);
+                l++;
             }
+            else {
+                if (rMax > height[r]) {
+                    water += min(height[l], rMax) - height[r];
+                }
 
-            prefix = max(prefix, height[i]);
+                rMax = max(rMax, height[r]);
+                r--;
+            }
         }
 
         return water;
