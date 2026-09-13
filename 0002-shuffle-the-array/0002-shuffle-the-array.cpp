8525a619115d7e6ecfbd71ee07674bd96182fa65
Girish Garg <girishprof65@gmail.com>
2026-09-14 01:42:26 +0530

    Time: 4 ms (65.34%), Space: 13.3 MB (97.38%) - LeetHub

--- 0002-shuffle-the-array/0002-shuffle-the-array.cpp
+++ 0002-shuffle-the-array/0002-shuffle-the-array.cpp
@@ -3,15 +3,10 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans(2 * n);
         int left = 0, right = n;
-        int i = 0;
-
-        while (left < n && right < 2 * n) {
-            ans[i] = nums[left];
-            ans[i + 1] = nums[right];
-
-            left++;
-            right++;
-            i += 2;
+        
+        for (int i = 0; i < 2 * n; i += 2) {
+            ans[i] = nums[left++];
+            ans[i + 1] = nums[right++];
         }
 
         return ans;
