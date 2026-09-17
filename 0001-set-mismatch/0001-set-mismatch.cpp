4ebbc78b91d21db5f5fce46dde7dbaafed78d1e9
Girish Garg <girishprof65@gmail.com>
2026-09-17 22:34:20 +0530

    Time: 4 ms (43.44%), Space: 26.4 MB (37.71%) - LeetHub

--- 0001-set-mismatch/0001-set-mismatch.cpp
+++ 0001-set-mismatch/0001-set-mismatch.cpp
@@ -4,23 +4,16 @@ public:
         int n = nums.size();
         vector<int> freq(n + 1, 0);
 
-        int og = n * (n + 1) / 2;
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-            freq[num]++;
+        for (int num : nums) freq[num]++;
+
+        int twice = -1;
+        int missing = -1;
+
+        for (int i = 1; i < n + 1; i++) {
+            if (freq[i] == 2) twice = i;
+            if (freq[i] == 0) missing = i;
         }
 
-        int twice = 0;
-
-        for (int i = 0; i < n + 1; i++) {
-            if (freq[i] == 2) {
-                twice = i;
-                break;
-            }
-        }
-
-        int missing = twice + og - sum;
         return {twice, missing};
     }
 };
