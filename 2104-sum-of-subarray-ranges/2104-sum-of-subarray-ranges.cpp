28dd454bdb9fd5460067b409a9798299b3817d25
Girish Garg <girishprof65@gmail.com>
2026-09-20 15:28:42 +0530

    2104-sum-of-subarray-ranges Medium 09-20-2026 0 ms (100%)

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,20 +2,44 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
         
+        stack<int> st;
+        stack<int> st2;
+        vector<int> prevSmall(n), nextSmall(n), prevGreat(n), nextGreat(n);
+
         for (int i = 0; i < n; i++) {
-            int maxi = nums[i];
-            int mini = nums[i];
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            while (!st2.empty() && nums[st2.top()] <= nums[i]) st2.pop();
 
-            for (int j = i + 1; j < n; j++) {
-                maxi = max(maxi, nums[j]);
-                mini = min(mini, nums[j]);
+            prevSmall[i] = st.empty() ? -1 : st.top();
+            prevGreat[i] = st2.empty() ? -1 : st2.top();
 
-                ans += maxi - mini;
-            }
+            st2.push(i);
+            st.push(i);
         }
 
-        return ans;
+        while (!st.empty()) st.pop();
+        while (!st2.empty()) st2.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            while (!st2.empty() && nums[st2.top()] < nums[i]) st2.pop();
+
+            nextSmall[i] = st.empty() ? n : st.top();
+            nextGreat[i] = st2.empty() ? n : st2.top();
+
+            st.push(i);
+            st2.push(i);
+        }
+
+        long long maxi = 0;
+        long long mini = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxi += 1LL * (nextGreat[i] - i) * (i - prevGreat[i]) * nums[i];
+            mini += 1LL * (nextSmall[i] - i) * (i - prevSmall[i]) * nums[i];
+        }
+
+        return maxi - mini;
     }
 };
