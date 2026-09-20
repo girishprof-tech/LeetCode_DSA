77ce99a920f8043885829b9c49e17ed745462553
Girish Garg <girishprof65@gmail.com>
2026-09-20 10:35:04 +0530

    3498-reverse-degree-of-a-string Easy 09-20-2026 0 ms (100%)

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int reverseDegree(string s) {
+        int n = s.length();
         int ans = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < n; i++) {
             ans += ((26 - s[i] + 'a') * (i + 1));
         }
 
