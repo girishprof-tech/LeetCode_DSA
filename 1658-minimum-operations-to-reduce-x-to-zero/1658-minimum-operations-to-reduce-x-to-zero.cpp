9483fe70d1338b3fe2231e0e3c390ba34de3377a
Girish Garg <girishprof65@gmail.com>
2026-09-23 14:46:47 +0530

    1658-minimum-operations-to-reduce-x-to-zero Medium 09-23-2026 4 ms (60.6%)

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,36 +4,27 @@ public:
         int n = nums.size();
         if (nums[0] > x && nums[n - 1] > x) return -1;
         if (nums[0] == x || nums[n - 1] == x) return 1;
-
         int total = accumulate(nums.begin(), nums.end(), 0);
-
         if (total < x) return -1;
+        if (total == x) return n;
 
-        vector<pair<int, int>> subarrays;
-        int req = total - x;
         int l = 0;
-
         long long sum = 0;
+        int mx = 0;
         for (int r = 0; r < n; r++) {
             sum += nums[r];
 
-            while (sum > req) {
+            while (sum > total - x) {
                 sum -= nums[l];
                 l++;
             }
 
-            if (sum == req) {
-                subarrays.push_back({l, r});
+            if (sum == total - x) {
+                mx = max(mx, r - l + 1);
             }
         }
-        
-        int m = subarrays.size();
-        if (m == 0) return -1;
 
-        int mx = 0;
-        for (int i = 0; i < m; i++) {
-            mx = max(mx, subarrays[i].second - subarrays[i].first + 1);
-        }
+        if (mx == 0) return -1;
 
         return n - mx;
     }
