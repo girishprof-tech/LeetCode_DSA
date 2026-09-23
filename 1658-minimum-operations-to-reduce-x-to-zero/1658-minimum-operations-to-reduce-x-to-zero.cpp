f29aaca5952f915e6b5cad416c30a3c950f61798
Girish Garg <girishprof65@gmail.com>
2026-09-23 14:41:33 +0530

    1658-minimum-operations-to-reduce-x-to-zero Medium 09-23-2026 16 ms (24.92%)

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        if (nums[0] > x && nums[n - 1] > x) return -1;
+        if (nums[0] == x || nums[n - 1] == x) return 1;
+
+        int total = 0;
+        for (int el : nums) {
+            total += el;
+        }
+
+        if (total < x) return -1;
+
+        vector<pair<int, int>> subarrays;
+        int req = total - x;
+        int l = 0;
+
+        long long sum = 0;
+        for (int r = 0; r < n; r++) {
+            sum += nums[r];
+
+            while (sum > req) {
+                sum -= nums[l];
+                l++;
+            }
+
+            if (sum == req) {
+                subarrays.push_back({l, r});
+            }
+        }
+        
+        int m = subarrays.size();
+        if (m == 0) return -1;
+
+        int mx = 0;
+        for (int i = 0; i < m; i++) {
+            mx = max(mx, subarrays[i].second - subarrays[i].first + 1);
+        }
+
+        return n - mx;
+    }
+};
