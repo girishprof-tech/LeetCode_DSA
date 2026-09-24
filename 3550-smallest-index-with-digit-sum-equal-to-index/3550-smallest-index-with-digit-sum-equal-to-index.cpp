4b8474c4726cdea308bfa1c33ad633647ff9b04f
Girish Garg <girishprof65@gmail.com>
2026-09-24 11:40:44 +0530

    3550-smallest-index-with-digit-sum-equal-to-index Easy 09-24-2026 0 ms (100%)

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -14,7 +14,13 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            int sum = digitSum(nums[i]);
+            int sum = 0;
+            int num = nums[i];
+            while (num > 0) {
+                int rem = num % 10;
+                sum += rem;
+                num /= 10;
+            }
 
             if (sum == i) return i;
         }
